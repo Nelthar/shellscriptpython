b2463fea37a3e28936aa511f30a243f7fdf7882a
Neltharion Deathwing <nelthar@protonmail.com>
2016-11-09 16:20:29 -0200

    Update to py 3.x

--- linux1.py
+++ linux1.py
@@ -8,42 +8,41 @@
 import os
 
 #relação com os comandos deste sistema
-import commands 
+import subprocess 
  
 #variável var pegará o return do comando executado (0 se deu certo o comando e diferente de 0 se deu errado)
 
 var = os.system("sudo apt-get update")
-print var
+print(var)
 
 #atualiza a lista de pacotes disponíveis baseando-se nas fontes listadas no arquivo list-sources
 var = os.system("sudo apt-get update") 
-print var
+print(var)
 
 #atualiza pacotes
 var = os.system("sudo apt-get upgrade -s") 
-print var
+print(var)
 
 #verifica se existem dependências erradas
 var = os.system("sudo apt-get check")
-print var
+print(var)
 
 #instala novos pacotes
 var = os.system("sudo apt-get -f install -s") 
-print var
+print(var)
 
 #apaga pastas e diretórios obtidos por downlad durante a instalação de pacotes
 var = os.system("sudo apt-get clean")
-print var
+print(var)
 
 #remove automaticamente todos os pacotes não utilizados do sistema, útil após realizar várias atualizações pois deixa o sistema mais limpo
 var = os.system("sudo apt-get autoremove -s")
-print var
+print(var)
 
 #remove automaticamente todos os pacotes não utilizados do sistema, útil após realizar várias atualizações pois deixa o sistema mais limpo
 var = os.system("sudo apt-get autoclean -s")
-print var
+print(var)
 
 #remove pacotes
 var = os.system("sudo apt-get -f remove -s")
-print var
-
+print(var)
